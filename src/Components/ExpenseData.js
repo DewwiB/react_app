1319f5f268659e8f5ae6b15e797a8a25137c2156
Dewi <dewwi@hotmail.co.uk>
2022-10-19 14:59:18 +0100

    test

--- src/Components/ExpenseData.js
+++ src/Components/ExpenseData.js
@@ -10,6 +10,7 @@ export default function ExpenseDate(props){
             <div>{month}</div>
             <div>{day}</div>
             <div>{year}</div>
+            <div>test</div>
         </div>
     );
 }
