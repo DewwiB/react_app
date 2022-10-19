50843565b97bf0bda674530b6e8614dc226f6303
dewi.brightman <dewi.brightman@proact.net>
2022-10-19 11:37:46 +0100

    fixed that isos issue

--- src/App.js
+++ src/App.js
@@ -22,13 +22,14 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  
   return (
     <div>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -1,9 +1,10 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
+export default function ExpenseItem(props) {
+  
   return (
     <div className="expense-item">
-      <div>{props.date.toISOSString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
@@ -12,4 +13,4 @@ function ExpenseItem(props) {
   );
 }
 
-export default ExpenseItem;
+
