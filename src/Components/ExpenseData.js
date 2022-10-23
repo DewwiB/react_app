053201ec921e200a85bac080c9b2ae85fc5be4f2
dewi <dewwi@hotmail.co.uk>
2022-10-23 17:53:39 +0100

    delete burger

--- src/Components/ExpenseData.js
+++ src/Components/ExpenseData.js
@@ -10,7 +10,6 @@ export default function ExpenseDate(props) {
       <div className="expense-date__month">{month}</div>
       <div className="expense-date__year">{year}</div>
       <div className="expense-date__day">{day}</div>
-      <div>hamburger</div>
     </div>
   );
 }
