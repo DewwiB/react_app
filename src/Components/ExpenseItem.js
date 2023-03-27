5a15d85d900403fe352e934d72cc3f14bbf4f0f4
dewi <dewwi@hotmail.co.uk>
2023-03-27 08:31:06 +0100

    new start!

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,14 +1,12 @@
-
-import './ExpenseItem.css';
-import './ExpenseData';
-import ExpenseDate from './ExpenseData';
-import Card from './Card';
+import "./ExpenseItem.css";
+import "./ExpenseDate";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 export default function ExpenseItem(props) {
-  
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>   
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
@@ -16,5 +14,3 @@ export default function ExpenseItem(props) {
     </Card>
   );
 }
-
-
