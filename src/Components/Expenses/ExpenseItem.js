11ef1e15e8a34a425889f02d3d66c6cc7d76414c
dewi <dewwi@hotmail.co.uk>
2023-07-06 16:45:17 +0100

    don't know what happened lol

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -16,7 +16,7 @@ export default function ExpenseItem(props) {
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">£{props.amount}</div>
             </div>
         </Card>
